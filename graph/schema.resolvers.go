bac66cc19f98779f824b0319fd1da7f8e56854f2
leonardodelira <leonardo.lira@hotmail.com.br>
2022-08-26 11:05:37 -0300

    feat: apply resolvers

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,40 +5,81 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/leonardodelira/golang-graphql-test/graph/generated"
 	"github.com/leonardodelira/golang-graphql-test/graph/model"
 )
 
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented: CreateCategory - createCategory"))
+	category := &model.Category{
+		ID:          uuid.New().String(),
+		Name:        input.Name,
+		Description: &input.Description,
+	}
+	r.Resolver.Categories = append(r.Resolver.Categories, category)
+	return category, nil
 }
 
 // CreateCourse is the resolver for the createCourse field.
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
-	panic(fmt.Errorf("not implemented: CreateCourse - createCourse"))
+	var category = &model.Category{}
+
+	for _, v := range r.Resolver.Categories {
+		if v.ID == input.CategoryID {
+			category = v
+			break
+		}
+	}
+
+	course := &model.Course{
+		ID:          uuid.New().String(),
+		Name:        input.Name,
+		Description: &input.Description,
+		Category:    category,
+	}
+
+	r.Resolver.Courses = append(r.Resolver.Courses, course)
+
+	return course, nil
 }
 
 // CreateChapter is the resolver for the createChapter field.
 func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapter) (*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented: CreateChapter - createChapter"))
+	var course = &model.Course{}
+
+	for _, v := range r.Resolver.Courses {
+		if v.ID == input.CourseID {
+			course = v
+			break
+		}
+	}
+
+	chapter := &model.Chapter{
+		ID:     uuid.New().String(),
+		Name:   input.Name,
+		Course: course,
+	}
+
+	r.Resolver.Chapters = append(r.Resolver.Chapters, chapter)
+
+	return chapter, nil
 }
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	return r.Resolver.Categories, nil
 }
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	return r.Resolver.Courses, nil
 }
 
 // Chapters is the resolver for the chapters field.
 func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented: Chapters - chapters"))
+	return r.Resolver.Chapters, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
